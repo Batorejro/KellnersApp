a44e799c8568093aba25b1bcee44d38fc70967d5
Arkadiusz Pedzisz <serwis.artech@gmail.com>
2025-03-14 00:08:44 +0100

    first commit with all component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
-function App() {
+import { Container } from 'react-bootstrap';
+
+import { Routes, Route } from 'react-router-dom';
+
+import { Home } from './components/pages/Home';
+import { NotFound } from './components/pages/NotFound';
+import { SingleTable } from './components/features/SingleTable';
+import { Header } from './components/views/Header';
+import { Footer } from './components/views/Footer';
+
+import { fetchData } from './redux/tablesReducer';
+
+export const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/table/:id' element={<SingleTable />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </Container>
   );
-}
-
-export default App;
+};
